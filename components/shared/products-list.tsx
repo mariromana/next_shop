be0ae520cc6195b090185cce4e5fdfe6aa746ae6
mari <mariroman604@gmail.com>
2024-09-08 22:47:55 +0200

    first commit

---
+++ components/shared/products-list.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import { Title } from './title';
+import { cn } from '@/lib/utils';
+import { ProductCard } from './product-card';
+import { useIntersection } from 'react-use';
+import { useCategoryStore } from '@/store/category';
+interface Props {
+    title: string;
+    items: any[];
+    listClassName?: string;
+    categoryId: number;
+    className?: string;
+}
+
+export const ProductList: React.FC<Props> = ({
+    title,
+    items,
+    listClassName,
+    categoryId,
+    className,
+}) => {
+    const setActiveCategory = useCategoryStore((state) => state.setActiveId);
+    const intersectionRef = React.useRef(null);
+    const intersection = useIntersection(intersectionRef, {
+        threshold: 0.4,
+    });
+
+    React.useEffect(() => {
+        if (intersection?.isIntersecting) {
+            setActiveCategory(categoryId);
+        }
+    }, [intersection?.isIntersecting, categoryId, title]);
+    return (
+        <div className={className} id={title} ref={intersectionRef}>
+            <Title text={title} size="lg" className="font-extrabold mb-5" />
+
+            <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+                {items.map((item) => (
+                    <ProductCard
+                        key={item.id}
+                        id={item.id}
+                        name={item.name}
+                        price={item.items[0].price}
+                        imageUrl={item.imageUrl}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
---
+++ services/products.ts
@@ -0,0 +1,10 @@
+import { Product } from '@prisma/client';
+import { axiosInstance } from './instance';
+
+export const search = async (query: string) => {
+    return (
+        await axiosInstance.get<Product>('/products/search', {
+            params: { query },
+        })
+    ).data;
+};
