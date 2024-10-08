af2fc435445bbaa456a5a8bc1ff18d6bcd483fb4
mari <mariroman604@gmail.com>
2024-10-08 11:35:48 +0200

    addGoogleAuthorization

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -4,9 +4,13 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { prisma } from '@/prisma/prisma-client';
 import { compare, hashSync } from 'bcrypt';
 import { UserRole } from '@prisma/client';
-
+import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: AuthOptions = {
     providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID || '',
             clientSecret: process.env.GITHUB_SECRET || '',
