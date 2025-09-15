4768060395de0d2a4b861dc9813b50bafb19734a
mari <mariroman604@gmail.com>
2025-09-15 16:26:42 +0200

    change package.json

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/shared/components/shared';
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -15,7 +16,10 @@ export default function HomeLayout({
 }>) {
     return (
         <main className="min-h-screen">
-            <Header />
+            <Suspense>
+                <Header />
+            </Suspense>
+
             {children}
             {modal}
         </main>
